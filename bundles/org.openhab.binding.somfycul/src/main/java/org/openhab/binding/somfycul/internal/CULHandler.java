c72336dd9849e3967daf45da43dfe5c0f8e41b0d
Nookyx <shox06@gmx.de>
2024-03-22 00:46:43 +0100

    revert CULHandler writeString buffering

--- bundles/org.openhab.binding.somfycul/src/main/java/org/openhab/binding/somfycul/internal/CULHandler.java
+++ bundles/org.openhab.binding.somfycul/src/main/java/org/openhab/binding/somfycul/internal/CULHandler.java
@@ -15,17 +15,12 @@ package org.openhab.binding.somfycul.internal;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TooManyListenersException;
 import java.util.stream.Collectors;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.io.transport.serial.PortInUseException;
 import org.openhab.core.io.transport.serial.SerialPort;
-import org.openhab.core.io.transport.serial.SerialPortEvent;
-import org.openhab.core.io.transport.serial.SerialPortEventListener;
 import org.openhab.core.io.transport.serial.SerialPortIdentifier;
 import org.openhab.core.io.transport.serial.SerialPortManager;
 import org.openhab.core.io.transport.serial.UnsupportedCommOperationException;
@@ -108,52 +103,12 @@ public class CULHandler extends BaseBridgeHandler {
             }
         }
         try {
-            final List<Boolean> listenerResult = new ArrayList<>();
-            serialPort.addEventListener(new SerialPortEventListener() {
-                @Override
-                public void serialEvent(SerialPortEvent event) {
-                    if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-                        final StringBuilder sb = new StringBuilder();
-                        final byte[] readBuffer = new byte[20];
-                        try {
-                            do {
-                                while (inputStream.available() > 0) {
-                                    final int bytes = inputStream.read(readBuffer);
-                                    sb.append(new String(readBuffer, 0, bytes));
-                                }
-                                try {
-                                    // add wait states around reading the stream, so that interrupted transmissions are
-                                    // merged
-                                    Thread.sleep(100);
-                                } catch (InterruptedException e) {
-                                    // ignore interruption
-                                }
-                            } while (inputStream.available() > 0);
-                            final String result = sb.toString();
-                            if (result.equals(msg)) {
-                                listenerResult.add(true);
-                            }
-                        } catch (IOException e) {
-                            logger.debug("Error receiving data on serial port {}: {}", portId.getName(),
-                                    e.getMessage());
-                        }
-                    }
-                }
-            });
-            serialPort.notifyOnDataAvailable(true);
-            outputStream.write(msg.getBytes());
+            outputStream.write((msg + "\n").getBytes());
             outputStream.flush();
             lastCommandTime = System.currentTimeMillis();
-            final long timeout = lastCommandTime + 1000;
-            while (listenerResult.isEmpty() && System.currentTimeMillis() < timeout) {
-                // Waiting for response
-                Thread.sleep(100);
-            }
-            return !listenerResult.isEmpty();
-        } catch (IOException | TooManyListenersException | InterruptedException e) {
+            return true;
+        } catch (IOException e) {
             logger.error("Error writing '{}' to serial port {}: {}", msg, portId.getName(), e.getMessage());
-        } finally {
-            serialPort.removeEventListener();
         }
         return false;
     }
